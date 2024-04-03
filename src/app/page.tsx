36982d422415191bc88c4ef0897db703b7b9e679
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-04 00:41:40 +0530

    add basic WIP page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div className="flex h-screen flex-col items-center justify-between">App Content</div>
+  return (
+    <div className="flex h-screen place-content-center items-center bg-black ">
+      <p className="text-lg text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        Work In Progress
+      </p>
+    </div>
+  )
 }
 
 export default App
