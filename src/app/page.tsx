99177fe7fb061200ba1f022b4fd4993d5e2fbedd
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-04 22:40:41 +0530

    add a skeletonlanding page with preline ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,42 @@
 const App = () => {
   return (
-    <div className="flex h-screen place-content-center items-center bg-black ">
-      <p className="text-lg text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
-        Work In Progress
-      </p>
+    <div className="bg-gradient-to-b from-violet-600/[.15] via-transparent">
+      <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-24 space-y-8">
+        <div className="max-w-3xl text-center mx-auto">
+          <h1 className="block font-medium text-gray-200 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Single solution to all of your problems.
+          </h1>
+        </div>
+
+        <div className="max-w-3xl text-center mx-auto">
+          <p className="text-lg text-gray-400">
+            SmartMaker is a Nagpur based company that offers solution to your electrical needs.
+          </p>
+        </div>
+
+        <div className="text-center">
+          <a
+            className="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 shadow-lg shadow-transparent hover:shadow-blue-700/50 border border-transparent text-white text-sm font-medium rounded-full focus:outline-none focus:ring-1 focus:ring-gray-600 py-3 px-6 dark:focus:ring-offset-gray-800"
+            href="#"
+          >
+            Browse Catalog
+            <svg
+              className="flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ const config: Config = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    'node_modules/preline/dist/*.js'
   ],
   theme: {
     extend: {
@@ -14,6 +15,6 @@ const config: Config = {
       }
     }
   },
-  plugins: []
+  plugins: [require('preline/plugin')]
 }
 export default config
