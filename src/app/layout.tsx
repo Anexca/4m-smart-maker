0462c6e2e318a5d9d15bd7e1fcb9a2333eef01ed
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-04 23:25:54 +0530

    load preline script asynchronously

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
 
-      <script src="./node_modules/preline/dist/preline.js"></script>
+      <script src="./node_modules/preline/dist/preline.js" async></script>
     </html>
   )
 }
