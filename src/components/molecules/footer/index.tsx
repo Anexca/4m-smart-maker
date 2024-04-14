1b7e6a8ea8370d52731e72022178e445d7af0f6a
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-15 00:14:52 +0530

    change dark classes

--- src/components/molecules/footer/index.tsx
+++ src/components/molecules/footer/index.tsx
@@ -13,11 +13,11 @@ const Footer = () => {
             className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
           >
             <Image src={logoImage} className="h-10 w-16" width={100} height={100} alt="4M Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
               4M Smart Maker
             </span>
           </a>
-          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium m:mb-0 text-gray-400">
             <li>
               <a href="#" className="hover:underline me-4 md:me-6">
                 About
@@ -30,8 +30,8 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
+        <span className="block text-sm sm:text-center text-gray-400">
           © {new Date().getFullYear()} 4M
         </span>
       </div>
