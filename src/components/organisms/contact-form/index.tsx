f1856fb575481a43e1308a10e1461f2311509e69
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-17 00:21:57 +0530

    update form input text color

--- src/components/organisms/contact-form/index.tsx
+++ src/components/organisms/contact-form/index.tsx
@@ -12,7 +12,7 @@ const ContactForm = () => {
               id="name"
               autoComplete="given-name"
               placeholder="Your name"
-              className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-300 sm:mb-0"
+              className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-900 sm:mb-0"
               name="name"
             />
           </div>
@@ -23,7 +23,7 @@ const ContactForm = () => {
               id="email"
               autoComplete="email"
               placeholder="Your email address"
-              className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-300 sm:mb-0"
+              className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-900 sm:mb-0"
               name="email"
             />
           </div>
@@ -36,15 +36,12 @@ const ContactForm = () => {
             cols={30}
             rows={5}
             placeholder="Write your message..."
-            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-300 sm:mb-0"
+            className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md text-gray-900 sm:mb-0"
           ></textarea>
         </div>
       </div>
       <div className="text-center">
-        <button
-          type="submit"
-          className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0"
-        >
+        <button className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0">
           Send Message
         </button>
       </div>
