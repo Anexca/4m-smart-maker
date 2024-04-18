343fdab9ec7ad4ccc52d49398e8a3d5328dc1875
nullsploit01 <harshaldharmik099@gmail.com>
2024-04-18 23:18:10 +0530

    add emailjs

---
+++ src/client/email/index.ts
@@ -0,0 +1,21 @@
+// import emailjs from '@emailjs/browser'
+
+// class EmailClient {
+//   sendEmail = async () => {
+//     return await emailjs
+//     .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, {
+//       publicKey: 'YOUR_PUBLIC_KEY',
+//     })
+//     .then(
+//       () => {
+//         console.log('SUCCESS!');
+//       },
+//       (error) => {
+//         console.log('FAILED...', error.text);
+//       },
+//     );
+// };
+//   }
+// }
+
+// export const emailClient = new EmailClient()
---
+++ src/config/environment/index.ts
@@ -0,0 +1,4 @@
+export const Environment = {
+  EMAIL_JS_PUBLIC_KEY: process.env.EMAIL_JS_PUBLIC_KEY,
+  EMAIL_JS_SERVICE_ID: process.env.EMAIL_JS_SERVICE_ID
+}
